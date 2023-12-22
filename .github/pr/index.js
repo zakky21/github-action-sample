610855de6c9fde8afccfc0489f96852187a2e052
Masanori OKAZAKI <okazaki@diggle.team>
2023-12-22 13:35:16 +0900

    append compare

--- .github/pr/index.js
+++ .github/pr/index.js
@@ -1,9 +1,45 @@
 const fs = require('fs')
+const https = require('https')
 
 console.log("start scripts")
 Object.keys(process.env).forEach(key => {
-  // console.log(key, process.env[key])
+  console.log(key, process.env[key])
 })
 
 console.log("read file...>>>>")
 console.log(JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH)))
+
+const { GITHUB_API_URL, GITHUB_REPOSITORY } = process.env
+const req = https.request({
+  hostname: GITHUB_API_URL,
+  path: `repo/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
+  method: 'GET',
+  headers: {
+    'Accept': 'application/vnd.github.v3.diff',
+    Authorization: `token ${this.base64User()}`,
+    'Content-Type': 'application/json',
+  },
+  timeout: 5000
+}, res => {
+  let chunks = [];
+  res.on('data', chunk => { chunks.push(chunk); })
+  res.on('end', () => {
+    const resb = Buffer.concat(chunks).toString();
+    try {
+      console.log("response", resb)
+      const json = JSON.parse(resb);
+      console.log("response-json", json)
+      resolve(json);
+    } catch (e) {
+      console.error(e);
+      reject(e);
+    }
+  });
+  res.on('error', err => reject(err))
+});
+req.on('timeout', () => {
+  req.destroy();
+  reject(Error('timeout'));
+})
+req.on('error', () => { })
+req.end();
