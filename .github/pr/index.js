3f8460e086a5f318764fc5f58542126c045964c4
Masanori OKAZAKI <okazaki@diggle.team>
2023-12-22 14:08:55 +0900

    fix

--- .github/pr/index.js
+++ .github/pr/index.js
@@ -9,37 +9,48 @@ Object.keys(process.env).forEach(key => {
 console.log("read file...>>>>")
 console.log(JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH)))
 
-const { GITHUB_API_URL, GITHUB_REPOSITORY } = process.env
-const req = https.request({
-  hostname: GITHUB_API_URL,
-  path: `repo/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
-  method: 'GET',
-  headers: {
-    'Accept': 'application/vnd.github.v3.diff',
-    Authorization: `token ${this.base64User()}`,
-    'Content-Type': 'application/json',
-  },
-  timeout: 5000
-}, res => {
-  let chunks = [];
-  res.on('data', chunk => { chunks.push(chunk); })
-  res.on('end', () => {
-    const resb = Buffer.concat(chunks).toString();
-    try {
-      console.log("response", resb)
-      const json = JSON.parse(resb);
-      console.log("response-json", json)
-      resolve(json);
-    } catch (e) {
-      console.error(e);
-      reject(e);
-    }
-  });
-  res.on('error', err => reject(err))
-});
-req.on('timeout', () => {
-  req.destroy();
-  reject(Error('timeout'));
-})
-req.on('error', () => { })
-req.end();
+function get() {
+  return new Promise((resolve, reject) => {
+    const { GITHUB_API_URL, GITHUB_REPOSITORY } = process.env
+    const req = https.request({
+      hostname: GITHUB_API_URL,
+      path: `repo/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
+      method: 'GET',
+      headers: {
+        'Accept': 'application/vnd.github.v3.diff',
+        Authorization: `token ${this.base64User()}`,
+        'Content-Type': 'application/json',
+      },
+      timeout: 5000
+    }, res => {
+      let chunks = [];
+      res.on('data', chunk => { chunks.push(chunk); })
+      res.on('end', () => {
+        const resb = Buffer.concat(chunks).toString();
+        try {
+          console.log("response", resb)
+          const json = JSON.parse(resb);
+          console.log("response-json", json)
+          resolve(json);
+        } catch (e) {
+          console.error(e);
+          reject(e);
+        }
+      });
+      res.on('error', err => reject(err))
+    });
+    req.on('timeout', () => {
+      req.destroy();
+      reject(Error('timeout'));
+    })
+    req.on('error', () => { })
+    req.end();
+  })
+}
+
+async function callAPI() {
+  const response = await get()
+  console.log("response", response)
+}
+
+callAPI()
