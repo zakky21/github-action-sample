a91c33943cede1ff60e43b21af6f89cec7aad904
Masanori OKAZAKI <okazaki@diggle.team>
2023-12-22 17:07:47 +0900

    re-try api

--- .github/pr/index.js
+++ .github/pr/index.js
@@ -1,58 +1,60 @@
 // const { Toolkit } = require('actions-toolkit');
-// const fs = require('fs')
-// const https = require('https')
+const fs = require('fs')
+const https = require('https')
 
-// console.log("start scripts")
-// Object.keys(process.env).forEach(key => {
-//   console.log(key, process.env[key])
-// })
+console.log("start scripts")
+Object.keys(process.env).forEach(key => {
+  console.log(key, process.env[key])
+})
 
-// console.log("read file...>>>>")
-// console.log(JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH)))
+console.log("read file...>>>>")
+console.log(JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH)))
 
-// const { GITHUB_API_URL, GITHUB_REPOSITORY, GITHUB_BASE_REF, GITHUB_HEAD_REF, INPUT_TOKEN } = process.env
-// console.log({
-//   hostname: GITHUB_API_URL,
-//   path: `repo/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
-// })
+const { GITHUB_API_URL, GITHUB_REPOSITORY, GITHUB_BASE_REF, GITHUB_HEAD_REF, INPUT_TOKEN } = process.env
+console.log({
+  hostname: GITHUB_API_URL,
+  path: `repo/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
+})
 
-// function get() {
-//   return new Promise((resolve, reject) => {
-//     const req = https.request({
-//       hostname: GITHUB_API_URL,
-//       path: `repo/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
-//       method: 'GET',
-//       headers: {
-//         'Accept': 'application/vnd.github.v3.diff',
-//         Authorization: `token ${INPUT_TOKEN}`,
-//         'Content-Type': 'application/json',
-//       },
-//       timeout: 5000
-//     }, res => {
-//       let chunks = [];
-//       res.on('data', chunk => { chunks.push(chunk); })
-//       res.on('end', () => {
-//         const resb = Buffer.concat(chunks).toString();
-//         try {
-//           console.log("response", resb)
-//           const json = JSON.parse(resb);
-//           console.log("response-json", json)
-//           resolve(json);
-//         } catch (e) {
-//           console.error(e);
-//           reject(e);
-//         }
-//       });
-//       res.on('error', err => reject(err))
-//     });
-//     req.on('timeout', () => {
-//       req.destroy();
-//       reject(Error('timeout'));
-//     })
-//     req.on('error', () => { })
-//     req.end();
-//   })
-// }
+function get() {
+  return new Promise((resolve, reject) => {
+    const req = https.request({
+      hostname: GITHUB_API_URL,
+      path: `repo/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
+      method: 'GET',
+      headers: {
+        'Accept': 'application/vnd.github.v3.diff',
+        Authorization: `token ${INPUT_TOKEN}`,
+        // 'Content-Type': 'application/json',
+      },
+      timeout: 5000
+    }, res => {
+      let chunks = [];
+      res.on('data', chunk => { console.log('data', chunk); chunks.push(chunk); })
+      res.on('end', () => {
+        console.log('end');
+        const resb = Buffer.concat(chunks).toString();
+        try {
+          console.log("response", resb)
+          const json = JSON.parse(resb);
+          console.log("response-json", json)
+          resolve(json);
+        } catch (e) {
+          console.error(e);
+          reject(e);
+        }
+      });
+      res.on('error', err => { console.log('error'); reject(err) })
+    });
+    req.on('timeout', () => {
+      log('timeout');
+      req.destroy();
+      reject(Error('timeout'));
+    })
+    req.on('error', () => { log('error2'); })
+    req.end();
+  })
+}
 
 // // const response = await get()
 // // console.log("response", response)
@@ -65,7 +67,7 @@ function aaaaa() {
 
 async function run() {
   console.log('called RUN!!!')
-  const response = await aaaaa()
+  const response = await get()
   console.log("response", response)
 }
 
