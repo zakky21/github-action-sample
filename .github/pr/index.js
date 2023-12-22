dffc7ef876f0775c85a82ce3dcde61d8a5d0bacd
Masanori OKAZAKI <okazaki@diggle.team>
2023-12-22 14:32:56 +0900

    add promise

--- .github/pr/index.js
+++ .github/pr/index.js
@@ -16,47 +16,47 @@ console.log({
   path: `repo/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
 })
 
-// function get() {
-//   return new Promise((resolve, reject) => {
-const req = https.request({
-  hostname: GITHUB_API_URL,
-  path: `repo/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
-  method: 'GET',
-  headers: {
-    'Accept': 'application/vnd.github.v3.diff',
-    Authorization: `token ${INPUT_TOKEN}`,
-    'Content-Type': 'application/json',
-  },
-  timeout: 5000
-}, res => {
-  let chunks = [];
-  res.on('data', chunk => { chunks.push(chunk); })
-  res.on('end', () => {
-    const resb = Buffer.concat(chunks).toString();
-    try {
-      console.log("response", resb)
-      const json = JSON.parse(resb);
-      console.log("response-json", json)
-      resolve(json);
-    } catch (e) {
-      console.error(e);
-      reject(e);
-    }
-  });
-  res.on('error', err => reject(err))
-});
-req.on('timeout', () => {
-  req.destroy();
-  reject(Error('timeout'));
-})
-req.on('error', () => { })
-req.end();
-//   })
-// }
+function get() {
+  return new Promise((resolve, reject) => {
+    const req = https.request({
+      hostname: GITHUB_API_URL,
+      path: `repo/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
+      method: 'GET',
+      headers: {
+        'Accept': 'application/vnd.github.v3.diff',
+        Authorization: `token ${INPUT_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      timeout: 5000
+    }, res => {
+      let chunks = [];
+      res.on('data', chunk => { chunks.push(chunk); })
+      res.on('end', () => {
+        const resb = Buffer.concat(chunks).toString();
+        try {
+          console.log("response", resb)
+          const json = JSON.parse(resb);
+          console.log("response-json", json)
+          resolve(json);
+        } catch (e) {
+          console.error(e);
+          reject(e);
+        }
+      });
+      res.on('error', err => reject(err))
+    });
+    req.on('timeout', () => {
+      req.destroy();
+      reject(Error('timeout'));
+    })
+    req.on('error', () => { })
+    req.end();
+  })
+}
 
-// async function callAPI() {
-//   const response = await get()
-//   console.log("response", response)
-// }
+async function callAPI() {
+  const response = await get()
+  console.log("response", response)
+}
 
-// callAPI()
+callAPI()
