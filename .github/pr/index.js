4f81ad340e32e255149661807c3e80fcda07bffc
Masanori OKAZAKI <okazaki@diggle.team>
2023-12-22 12:07:39 +0900

    add comment

--- .github/pr/index.js
+++ .github/pr/index.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 
 console.log("start scripts")
 Object.keys(process.env).forEach(key => {
-  console.log(key, process.env[key])
+  // console.log(key, process.env[key])
 })
 
+console.log("read file...>>>>")
 console.log(JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH)))
