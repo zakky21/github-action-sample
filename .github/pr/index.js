6635215ee3c943fd7d2f11a2d907a790aa18c450
Masanori OKAZAKI <okazaki@diggle.team>
2023-12-28 11:04:11 +0900

    fix

--- .github/pr/index.js
+++ .github/pr/index.js
@@ -2,18 +2,24 @@ const fs = require('fs')
 const https = require('https')
 
 function commentPR(todos) {
-  const comments = todos.map(({ a, b, lines }) => {
+  const comments = todos.map(({ a, b, lines, todoLines }) => {
+
+    const detail = todoLines.map(({ index, type }) => {
+      const l = lines.substring(index - 5 < 0 ? 0 : index - 5, index + 10)
+      return `
+#### TODOが ${type === '+' ? '追加' : '削除'} されました
+\`\`\`
+${l.join('\n')}
+\`\`\`
+      `
+    })
     return `
 #### ${b}
-file: ${a} -> ${b}
-\`\`\`
-${lines.join('\n')}
-\`\`\`
-
+${detail}
     `
   })
 
-  const { GITHUB_API_URL, INPUT_COMMENT_URL, GITHUB_SHA, INPUT_TOKEN } = process.env
+  const { GITHUB_API_URL, INPUT_COMMENT_URL, INPUT_AFTER, INPUT_TOKEN } = process.env
   console.log({
     hostname: GITHUB_API_URL.replace("https://", ""),
     path: INPUT_COMMENT_URL.replace(GITHUB_API_URL, ''),
@@ -45,13 +51,12 @@ ${lines.join('\n')}
     })
     req.write(JSON.stringify({
       body: `
-\`\`\`
-${GITHUB_SHA}
+commit_id: ${INPUT_AFTER}
+
 ### TODOが見つかりました
 以下のTODOコメントの内容に問題がないか（このPR内で解消ができないか、後になって理解ができるか）を確認してください
 
 ${comments}
-\`\`\`
     `}))
     req.on('timeout', () => {
       console.log('timeout')
@@ -68,34 +73,33 @@ async function parseDiff(str) {
 
   const todos = []
   let current = {};
-  str.split('\n').forEach(s => {
+  str.split('\n').forEach((s, i) => {
     if (/^diff --git/.test(s)) {
       if (current.found) todos.push(current)
-      current = { lines: [] }
-    } else if (/^---/.test(s)) {
-      current.a = s
-    } else if (/^\+\+\+/.test(s)) {
-      current.b = s
+      current = { lines: [], todoLines: [] }
+    } else if (/^---\s/.test(s)) {
+      current.a = s.replace(/^---\s/, '')
+    } else if (/^\+\+\+\s/.test(s)) {
+      current.b = s.replace(/^\+\+\+\s/, '')
     } else if (/^@@.*@@/.test(s)) {
       if (current.found) todos.push(current)
-      current.lines = [s]
     } else {
-      if (/^\+.*(TODO|FIXME)/.test(s)) {
-        current.found = true
+      if (/^[+-].*(TODO|FIXME)/.test(s)) {
+        current.todoLines.push({ index: i, type: s[0] })
       }
-      current.lines.push(s)
     }
+    current.lines.push(s)
   })
   if (current.found) todos.push(current)
   if (todos.length) await commentPR(todos)
 }
 
 function getDiff() {
-  const { GITHUB_API_URL, GITHUB_REPOSITORY, GITHUB_BASE_REF, GITHUB_HEAD_REF, INPUT_TOKEN } = process.env
+  const { GITHUB_API_URL, GITHUB_REPOSITORY, INPUT_BEFORE, INPUT_AFTER, INPUT_TOKEN } = process.env
   return new Promise((resolve, reject) => {
     const req = https.request({
       hostname: GITHUB_API_URL.replace("https://", ""),
-      path: `/repos/${GITHUB_REPOSITORY}/compare/${GITHUB_BASE_REF}...${GITHUB_HEAD_REF}`,
+      path: `/repos/${GITHUB_REPOSITORY}/compare/${INPUT_BEFORE}...${INPUT_AFTER}`,
       method: 'GET',
       headers: {
         'Accept': 'application/vnd.github.v3.diff',
